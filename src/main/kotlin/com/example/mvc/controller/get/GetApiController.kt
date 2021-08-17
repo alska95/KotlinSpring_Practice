045a4ffd7f5165b57a9dd010d8f41a78daddeca4
alska95 <rhsalska55@naver.com>
2021-08-17 22:22:01 +0900

    GetMapping 정리

--- src/main/kotlin/com/example/mvc/controller/get/GetApiController.kt
+++ src/main/kotlin/com/example/mvc/controller/get/GetApiController.kt
@@ -1,15 +1,57 @@
 package com.example.mvc.controller.get
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import com.example.mvc.model.http.UserRequest
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api")
 class GetApiController {
 
-    @GetMapping("/hello")
+    @GetMapping(path = ["/hello" , "/abcd"])
     fun hello() : String{
         return "hello kotlin";
+    }
+
+    @RequestMapping(path = ["/request-mapping"] , method = [RequestMethod.GET])
+    fun requestMapping():String{
+        return "request-mapping";
+    }
+
+    @GetMapping("/get-mapping/path-variable/{name}/{age}")
+    fun pathVariable(@PathVariable name: String ,@PathVariable age:String): String {
+        println("${name} , ${age}")
+        return name;
+    }
+
+
+    @GetMapping("/get-mapping/path-variable2/{name}/{age}")
+    fun pathVariable2(@PathVariable(value = "name") _name: String ,@PathVariable age:String): String {
+        val name = "kotlin";
+
+        println("${name} , ${age}")
+        return name;
+    }
+
+    @GetMapping("/get-mapping/query-param")
+    fun queryParam(
+        @RequestParam name: String,
+        @RequestParam(value = "age") age:Int
+    ): String {
+        println("$name, $age")
+        return "$name $age";
+    }
+
+    @GetMapping("/get-mapping/query-param/object")
+    fun queryParamObject(@ModelAttribute userRequest: UserRequest) : UserRequest{
+        println(userRequest)
+        return userRequest
+    }
+
+    @GetMapping("/get-mapping/query-param/map")
+    fun queryParamMap(@RequestParam map : Map<String,Any>): Map<String, Any> {
+        print(map)
+        val phoneNumber = map.get("phone-number")
+        print(phoneNumber);
+        return map
     }
 }
